5d3ae80cc37a5e2a0844441326791b7a13d5c5df
Matloob333 <matloobahmad2469@gmail.com>
2025-07-08 01:44:03 +0530

    day1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import Message from "./Message.tsx";
+import Alert from "./component/Alert.tsx";
+import Button from "./component/Button.tsx";
 import ListGroup from "./component/ListGroup.tsx";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup />
+      {alertVisible && (
+        <Alert
+          onClose={() => {
+            setAlertVisibility(false);
+          }}
+        >
+          My Alert
+        </Alert>
+      )}
+
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        my button
+      </Button>
+
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
