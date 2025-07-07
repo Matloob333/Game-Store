c53d22d849614e034ce9f54d4f41e154a4662e19
Matloob333 <matloobahmad2469@gmail.com>
2025-07-07 22:47:30 +0530

    day1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Message.tsx";
+import ListGroup from "./component/ListGroup.tsx";
+
+function App() {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
+}
+export default App;
